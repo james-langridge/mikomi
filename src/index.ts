73e5ae0f4af2d178612b5564bc4371d677fc1ef6
James Langridge <l@ngridge.com>
2024-12-22 19:14:12 +0100

    Refactor potOdds()

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 /**
  * Calculates the percentage of time that a player must win for their call to be profitable.
- * @param potSize - The size of the pot
- * @param betSize - The size of the bet to call
+ * @param pot - The size of the pot
+ * @param bet - The size of the bet to call
  * @returns A percentage
  * @example
  * ```ts
  * potOdds(50, 30) // returns: 0.273
  * ```
  */
-export function potOdds(potSize: number, betSize: number) {
-  return betSize / potSize + betSize;
+export function potOdds(pot: number, bet: number) {
+  return Math.round((bet / (pot + 2 * bet)) * 1000) / 1000;
 }
