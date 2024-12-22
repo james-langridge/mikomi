73b2be1233068a989d403c6af6342737108e17f7
James Langridge <l@ngridge.com>
2024-12-22 23:24:12 +0100

    Add bluff option to potOdds()

--- README.md
+++ README.md
@@ -13,6 +13,9 @@ npm install mikomi
 ```js
 import { potOdds } from 'mikomi';
 
-const odds = potOdds({pot: 50, bet: 30});
-console.log(odds); // 0.273
+// 0.273 - must win at least 27.3 % of the time for call to be proftable
+const callOdds = potOdds({pot: 50, bet: 30});
+
+// 0.375 - bluff must be successful at least 37.5 % of the time to be profitable
+const bluffOdds = potOdds({pot: 50, bet: 30, isBuff: true})
 ```
--- src/pot-odds.ts
+++ src/pot-odds.ts
@@ -1,20 +1,25 @@
 export interface PotOddOptions {
   /** Size of the pot */
   pot: number;
-  /** Size of the bet to call */
+  /** Size of the call or bluff */
   bet: number;
+  /** Is this a bluff bet */
+  isBluff?: boolean;
 }
 
 /**
- * Calculates the percentage of time that a player must win for their call to be profitable.
+ * Calculates the percentage of time that a player must win for their call or bluff to be profitable.
  * @param pot - The size of the pot
  * @param bet - The size of the bet to call
+ * @param [isBluff] - Is this is a bluff bet (optional)
  * @returns A percentage to 3 decimal points
  * @example
  * ```ts
  * potOdds({pot: 50, bet: 30}) // returns: 0.273
+ * potOdds({pot: 50, bet: 30, isBuff: true}) // returns: 0.375
  * ```
  */
-export function potOdds({ pot, bet }: PotOddOptions) {
-  return Math.round((bet / (pot + 2 * bet)) * 1000) / 1000;
+export function potOdds({ pot, bet, isBluff = false }: PotOddOptions) {
+  const multiplier = isBluff ? 1 : 2;
+  return Math.round((bet / (pot + multiplier * bet)) * 1000) / 1000;
 }
